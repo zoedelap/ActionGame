087c8ddcbfc8cd507453acf89faaacaf0b4d4295
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-10-31 12:08:55 -0500

    detect collision with door if holding key

--- ActionGame/Assets/Scripts/KeyManager.cs
+++ ActionGame/Assets/Scripts/KeyManager.cs
@@ -6,12 +6,20 @@ using UnityEngine;
 
 public class KeyManager : MonoBehaviour
 {
+    private bool hasKey = false;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("key"))
         {
             print("entered key");
+            hasKey = true;
             Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("exit") && hasKey)
+        {
+            print("level complete");
         }
     }
 }
