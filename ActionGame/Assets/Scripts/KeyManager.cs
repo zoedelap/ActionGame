f6b44eed35894f8c66c5f5253242438a45388a89
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-10-31 12:20:34 -0500

    Added indicator to the screen

--- ActionGame/Assets/Scripts/KeyManager.cs
+++ ActionGame/Assets/Scripts/KeyManager.cs
@@ -1,19 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyManager : MonoBehaviour
 {
     private bool hasKey = false;
+    public Image uiIndicator;
 
-    private void OnTriggerEnter(Collider other)
+    void Start()
+    {
+        uiIndicator.enabled = false;    
+    }
+
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("key"))
         {
             print("entered key");
             hasKey = true;
+            uiIndicator.enabled = true;
             Destroy(other.gameObject);
         }
 
