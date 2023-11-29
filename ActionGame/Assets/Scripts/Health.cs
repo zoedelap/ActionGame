298efd49ac64c241bdd74e58865a7b8bbef65c15
Sean McLaughlin <smm6050@utulsa.edu>
2023-11-29 13:14:51 -0600

    add GUI health bar

---
+++ ActionGame/Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [Header("Health Settings")]
+    public int currHealth;
+    public int maxHealth;
+    public HealthBar healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = maxHealth;
+    }
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DamagePlayer(10);
+        }
+    }
+    void DamagePlayer(int dmg)
+    {
+        currHealth -= dmg;
+        healthBar.SetHealth(currHealth);
+    }
+    
+}
