0677b8b96d405f90dba0d062763b51c687a914f3
seanm603 <68670448+seanm603@users.noreply.github.com>
2023-12-07 13:26:45 -0600

    add pause and restart button

--- ActionGame/Assets/Scripts/UI/ButtonManager.cs
+++ ActionGame/Assets/Scripts/UI/ButtonManager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour
 {
     public GameObject instructionsPanel;
     public GameObject pausePanel;
+    public Button pauseButton;
 
     public void StartGame()
     {
@@ -17,11 +19,13 @@ public class ButtonManager : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }
-    public void ShowInstructions(){
+    public void ShowInstructions()
+    {
         instructionsPanel.SetActive(true);
     }
 
-    public void HideInstructions(){
+    public void HideInstructions()
+    {
         instructionsPanel.SetActive(false);
     }
 
@@ -29,17 +33,19 @@ public class ButtonManager : MonoBehaviour
     {
         Time.timeScale = 0;
         pausePanel.SetActive(true);
+        pauseButton.interactable = false;
     }
 
     public void ResumeGame()
     {
         Time.timeScale = 1;
         pausePanel.SetActive(false);
+        pauseButton.interactable = true;
     }
 
     public void RestartGame()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("level_1");
     }
 
     public void QuitGame()
